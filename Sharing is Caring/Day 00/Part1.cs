39be411e5f60a80820a70cebf6782d0b03291b91
Stefan van Oudenaarden <s.oudenaarden@gmail.com>
2020-12-02 21:16:53 +0100

    Template for Day Setup.

---
+++ Sharing is Caring/Day 00/Part1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using Serilog;
+using Advent;
+
+namespace Day_00
+{
+    //Problem URL
+    internal class Part1 : IAdventProblem
+    {
+        public string ProblemName { get => "Day 00: Something Something. Part One."; }
+
+        public void Run()
+        {
+            var inputList = Helpers.ReadStringsFile("Day 01/input.txt");
+        }
+    }
+}
---
+++ Sharing is Caring/Day 00/Part2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using Serilog;
+using Advent;
+
+namespace Day_00
+{
+    //Problem URL
+    public class Part2 : IAdventProblem
+    {
+        public string ProblemName { get => "Day 00: Something Something. Part Two."; }
+
+        public void Run()
+        {
+            var inputList = Helpers.ReadStringsFile("Day 01/input.txt");
+        }
+    }
+}
