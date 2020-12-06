96f6db93f67bdaa86b823c6b8f3109a302648797
Stefan van Oudenaarden <s.oudenaarden@gmail.com>
2020-12-06 22:56:37 +0100

    2020: Day 6t: Helping a Someone

--- 2020 All Days, Every Day/Day 06/AdventDay.cs
+++ 2020 All Days, Every Day/Day 06/AdventDay.cs
@@ -7,11 +7,13 @@ namespace Day_06
     {
         private IAdventProblem ProblemPart1;
         private IAdventProblem ProblemPart2;
+        private IAdventProblem Reddit1;
 
         public AdventDay()
         {
             ProblemPart1 = new Part1();
             ProblemPart2 = new Part2();
+            Reddit1 = new RedditorBasukun();
         }
 
         public void SolveProblems()
@@ -21,6 +23,9 @@ namespace Day_06
 
             Log.Information("Running '{ProblemName}'", ProblemPart2.ProblemName);
             ProblemPart2.Run();
+
+            Log.Information("Running '{ProblemName}'", Reddit1.ProblemName);
+            Reddit1.Run();
         }
     }
 }
---
+++ 2020 All Days, Every Day/Day 06/Redditor-Basukun.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using Serilog;
+using Advent;
+using System.Linq;
+
+namespace Day_06
+{
+    //https://adventofcode.com/2020/day/6#part2
+    public class RedditorBasukun : IAdventProblem
+    {
+        private string Dayname => Helpers.GetDayFromNamespace(this);
+        public string ProblemName { get => $"Day {Dayname}: [Redditor: Basukun] Custom Customs. Part Two."; }
+
+        public void Run()
+        {
+            var testinputList = ParseInput($"Day {Dayname}/inputTest.txt");
+            var total = PartTwo(testinputList);
+
+            var inputList = ParseInput($"Day {Dayname}/input.txt");
+            total = PartTwo(inputList);
+        }
+
+        public static int PartTwo(List<List<string>> list)
+        {
+            int totalYesses = 0;
+            int groupcount = 0;
+            int formcount = 0;
+
+            foreach (var group in list)
+            {
+                groupcount++;
+                List<char> groupYesses = new List<char>();
+                List<char> currentYesses = new List<char>();
+
+                foreach (var person in group)
+                {
+                    formcount++;
+                    var personYesses = person.ToCharArray().ToList();
+                    if (group.First() == person && groupYesses.Count == 0)
+                    {
+                        foreach (var yes in personYesses)
+                        {
+                            currentYesses.Add(yes);
+                            groupYesses = new List<char>(currentYesses);
+                        }
+                    }
+                    else
+                    {
+                        for (int i = 0; i < groupYesses.Count; i++)
+                        {
+                            if (!personYesses.Any(t => t.Equals(groupYesses[i])))
+                            {
+                                currentYesses.Remove(groupYesses[i]);
+                            }
+                        }
+                        groupYesses = new List<char>(currentYesses);
+                    }
+                }
+                totalYesses += groupYesses.Count();
+                if (totalYesses is 1017)
+                {
+                    Log.Information("{@group}", group);
+                }
+
+                //Log.Information("After {groupcount} groups and {formcount} forms the total is: {total}.", groupcount, formcount, totalYesses);
+            }
+
+            Log.Information("After {groupcount} groups and {formcount} forms the total is: {total}.", groupcount, formcount, totalYesses);
+
+            return totalYesses;
+        }
+
+        private List<List<string>> ParseInput(string filePath)
+        {
+            var data = File.ReadAllText(filePath);
+            var awnserCollections = new List<List<string>>();
+
+            var entries = data.Trim().Split("\r\n\r\n");
+
+            foreach (var entry in entries)
+            {
+                var awnserCollection = entry.Split(Environment.NewLine);
+
+                awnserCollections.Add(new List<string>(awnserCollection));
+            }
+
+            return awnserCollections;
+        }
+    }
+}
