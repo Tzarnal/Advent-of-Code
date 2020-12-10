75c7b5c5c6f3e376bb1ccf647ca0d0ad94c1d196
Stefan van Oudenaarden <s.oudenaarden@gmail.com>
2020-12-10 16:26:39 +0100

    Added randomizing a list.

--- Sharing is Caring/Extensions.cs
+++ Sharing is Caring/Extensions.cs
@@ -1,11 +1,13 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Advent
 {
-    static class Extensions
+    internal static class Extensions
     {
+        private static Random _genie = new Random();
+
         public static int Count(this string str, string comp)
         {
             int starLength = str.Length;
@@ -13,6 +15,19 @@ namespace Advent
             int finalLength = modified.Length;
 
             return (starLength - finalLength) / comp.Length;
+        }
+
+        public static void Shuffle<T>(this IList<T> list)
+        {
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = _genie.Next(n + 1);
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
         }
     }
 }
