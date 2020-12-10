25eb7d3c29b67388267cfc9ce59b33ea658a9d4a
Stefan van Oudenaarden <s.oudenaarden@gmail.com>
2020-12-10 09:54:40 +0100

    2020: Day 10

---
+++ 2020 All Days, Every Day/Day 10/Part1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using Serilog;
+using Advent;
+
+namespace Day_10
+{
+    //https://adventofcode.com/2020/day/10
+    public class Part1 : IAdventProblem
+    {
+        private string Dayname => Helpers.GetDayFromNamespace(this);
+        public string ProblemName { get => $"Day {Dayname}: Adapter Array. Part One."; }
+
+        public void Run()
+        {
+            var testinputList = ParseInput($"Day {Dayname}/inputTest.txt");
+            Solve(testinputList);
+
+            var inputList = ParseInput($"Day {Dayname}/input.txt");
+            Solve(inputList);
+        }
+
+        public void Solve(List<int> input)
+        {
+            input.Add(0);//Initial zero to avoid off by one
+            input = input.OrderBy(i => i).ToList();
+            input.Add(input.Max() + 3);//Final device is +3  jolts
+
+            var differences = input.Zip(input.Skip(1), (f, s) => s - f);
+            var threejolts = differences.Where(i => i == 3).Count();
+            var onejolts = differences.Where(i => i == 1).Count();
+
+            Log.Information("Found {onejolts} one jolt differences and {threejolts} three jolt differences. Product {product}",
+                onejolts, threejolts, onejolts * threejolts);
+        }
+
+        private List<int> ParseInput(string filePath)
+        {
+            return Helpers.ReadStringsFile(filePath).ConvertAll(i => int.Parse(i));
+        }
+    }
+}
---
+++ 2020 All Days, Every Day/Day 10/Part2.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using Serilog;
+using Advent;
+
+namespace Day_10
+{
+    //https://adventofcode.com/2020/day/10#part2
+    public class Part2 : IAdventProblem
+    {
+        private string Dayname => Helpers.GetDayFromNamespace(this);
+        public string ProblemName { get => $"Day {Dayname}:Adapter Array. Part Two."; }
+
+        private Dictionary<int, long> _memo;
+        private List<int> _adapters;
+
+        public void Run()
+        {
+            _adapters = ParseInput($"Day {Dayname}/inputTest.txt");
+            Solve();
+
+            _adapters = ParseInput($"Day {Dayname}/inputTest2.txt");
+            Solve();
+
+            _adapters = ParseInput($"Day {Dayname}/input.txt");
+            Solve();
+        }
+
+        public void Solve()
+        {
+            _adapters.Add(0);//Initial zero
+            _adapters = _adapters.OrderBy(i => i).ToList();
+            _adapters.Add(_adapters.Max() + 3);//Final device
+
+            _memo = new Dictionary<int, long>();
+
+            var arrangements = FindArrangements(0);
+
+            Log.Information("Total arrangements {arrangements}.", arrangements);
+        }
+
+        private long FindArrangements(int i)
+        {
+            long arrangements = 0;
+
+            if (_memo.ContainsKey(i))
+                return _memo[i];
+
+            if (i == _adapters.Count - 1)
+                return 1;
+
+            for (int j = i + 1; j < _adapters.Count; j++)
+            {
+                if (_adapters[j] - _adapters[i] <= 3)
+                {
+                    arrangements += FindArrangements(j);
+                }
+            }
+
+            _memo[i] = arrangements;
+
+            return arrangements;
+        }
+
+        private List<int> ParseInput(string filePath)
+        {
+            return Helpers.ReadStringsFile(filePath).ConvertAll(i => int.Parse(i));
+        }
+    }
+}
