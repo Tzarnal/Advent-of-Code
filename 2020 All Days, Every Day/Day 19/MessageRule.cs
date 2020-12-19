1142d0980dbc3afa43ebdb6ded54a69f2708835a
Stefan van Oudenaarden <s.oudenaarden@gmail.com>
2020-12-19 09:19:19 +0100

    2020: Day 19: Part 1

---
+++ 2020 All Days, Every Day/Day 19/MessageRule.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using RegExtract;
+using Serilog;
+
+namespace Day_19
+{
+    public interface IMessageRule
+    {
+        int Number { get; set; }
+        bool Ready { get; set; }
+        bool Recurses { get; set; }
+        HashSet<string> MatchValues { get; set; }
+
+        bool Matches(string input);
+
+        void Prepare(Dictionary<int, IMessageRule> OtherRules, int maxLength);
+    }
+
+    public class MessageRuleString : IMessageRule
+    {
+        public int Number { get; set; }
+        public bool Ready { get; set; }
+        public bool Recurses { get; set; }
+        public HashSet<string> MatchValues { get; set; }
+
+        public MessageRuleString(int number, string ruleInput)
+        {
+            Number = number;
+            MatchValues = new HashSet<string>();
+
+            var matchText = ruleInput.Extract<string>(@"(\w)+");
+            MatchValues.Add(matchText);
+
+            Ready = true;
+            Recurses = false;
+        }
+
+        public bool Matches(string input)
+        {
+            return MatchValues.Contains(input);
+        }
+
+        public void Prepare(Dictionary<int, IMessageRule> OtherRules, int maxLength)
+        {
+        }
+    }
+
+    public class MessageRuleOr : IMessageRule
+    {
+        public int Number { get; set; }
+        public bool Ready { get; set; }
+        public bool Recurses { get; set; }
+        public HashSet<string> MatchValues { get; set; }
+
+        private List<int> _leftDependantRules;
+        private List<int> _rightDependantRules;
+
+        public MessageRuleOr(int number, string ruleInput)
+        {
+            Number = number;
+            MatchValues = new HashSet<string>();
+
+            var chunks = ruleInput.Split("|");
+
+            _leftDependantRules = chunks[0].Split(" ", StringSplitOptions.RemoveEmptyEntries)
+               .Select(s => int.Parse(s)).ToList();
+
+            _rightDependantRules = chunks[1].Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(s => int.Parse(s)).ToList();
+        }
+
+        public bool Matches(string input)
+        {
+            return MatchValues.Contains(input);
+        }
+
+        public void Prepare(Dictionary<int, IMessageRule> OtherRules, int maxLength)
+        {
+            var matchValues = new HashSet<string> { "" };
+
+            if (_leftDependantRules.Any(d => OtherRules[d].Recurses) || _rightDependantRules.Any(d => OtherRules[d].Recurses))
+            {
+                Recurses = true;
+                Ready = true;
+                return;
+            }
+
+            if (!Ready && _leftDependantRules.All(l => OtherRules[l].Ready) && _rightDependantRules.All(r => OtherRules[r].Ready))
+            {
+                
+
+                foreach (var lRule in _leftDependantRules)
+                {
+                    matchValues = Combine(matchValues, OtherRules[lRule].MatchValues);
+                }
+
+                MatchValues.UnionWith(matchValues);
+
+                matchValues = new HashSet<string> { "" };
+
+                foreach (var rRule in _rightDependantRules)
+                {
+                    matchValues = Combine(matchValues, OtherRules[rRule].MatchValues);
+                }
+
+                MatchValues.UnionWith(matchValues);
+
+                Ready = true;
+            }
+        }
+
+        private HashSet<string> Combine(HashSet<string> established, HashSet<string> extra)
+        {
+            var output = new HashSet<string>();
+
+            foreach (var entry in established)
+            {
+                foreach (var add in extra)
+                {
+                    output.Add(entry + add);
+                }
+            }
+
+            return output;
+        }
+    }
+
+    public class MessageRuleAnd : IMessageRule
+    {
+        public int Number { get; set; }
+        public bool Ready { get; set; }
+        public bool Recurses { get; set; }
+
+        public HashSet<string> MatchValues { get; set; }
+
+        private List<int> _dependantRules;
+
+        public MessageRuleAnd(int number, string ruleInput)
+        {
+            Number = number;
+            MatchValues = new HashSet<string>();
+
+            _dependantRules = ruleInput.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(s => int.Parse(s)).ToList();
+        }
+
+        public bool Matches(string input)
+        {
+            return MatchValues.Contains(input);
+        }
+
+        public void Prepare(Dictionary<int, IMessageRule> OtherRules, int maxLength)
+        {
+            var matchValues = new HashSet<string> { "" };
+
+            if (_dependantRules.Any(d => OtherRules[d].Recurses))
+            {
+                Recurses = true;
+                Ready = true;
+                return;
+            }
+
+            if (!Ready && _dependantRules.All(d => OtherRules[d].Ready))
+            {
+                
+
+                foreach (var dRule in _dependantRules)
+                {
+                    matchValues = Combine(matchValues, OtherRules[dRule].MatchValues);
+                }
+
+                MatchValues = matchValues;
+                Ready = true;
+            }
+        }
+
+        private HashSet<string> Combine(HashSet<string> established, HashSet<string> extra)
+        {
+            var output = new HashSet<string>();
+
+            foreach (var entry in established)
+            {
+                foreach (var add in extra)
+                {
+                    output.Add(entry + add);
+                }
+            }
+
+            return output;
+        }
+    }
+}
---
+++ 2020 All Days, Every Day/Day 19/Part1.cs
@@ -0,0 +1,88 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using Serilog;
+using Advent;
+using RegExtract;
+
+namespace Day_19
+{
+    //https://adventofcode.com/2020/day/19
+    public class Part1 : IAdventProblem
+    {
+        private string Dayname => Helpers.GetDayFromNamespace(this);
+        public string ProblemName { get => $"Day {Dayname}: Monster Messages. Part One."; }
+
+        public void Run()
+        {
+            //var (rules, messages) = ParseInput($"Day {Dayname}/inputTest.txt");
+            //Solve(rules, messages);
+
+            var (rules, messages) = ParseInput($"Day {Dayname}/input.txt");
+            Solve(rules, messages);
+        }
+
+        public void Solve(Dictionary<int, string> RuleInput, List<string> Messages)
+        {
+            var Rules = new Dictionary<int, IMessageRule>();
+
+            var maxLength = Messages.Max(m => m.Length);
+
+            foreach (var ruleInput in RuleInput)
+            {
+                if (ruleInput.Value.Contains("|"))
+                {
+                    Rules[ruleInput.Key] = new MessageRuleOr(ruleInput.Key, ruleInput.Value);
+                }
+                else if (ruleInput.Value.Contains("\""))
+                {
+                    Rules[ruleInput.Key] = new MessageRuleString(ruleInput.Key, ruleInput.Value);
+                }
+                else
+                {
+                    Rules[ruleInput.Key] = new MessageRuleAnd(ruleInput.Key, ruleInput.Value);
+                }
+            }
+
+            while (!Rules.All(r => r.Value.Ready))
+            {
+                foreach (var rule in Rules.Where(r => !r.Value.Ready))
+                {
+                    rule.Value.Prepare(Rules, maxLength);
+                }
+            }
+
+            var correctMessages = 0;
+            foreach (var message in Messages)
+            {
+                if (Rules[0].Matches(message))
+                {
+                    correctMessages++;
+                }
+            }
+
+            Log.Information("In  {count} Messages {correctMessages} matched.",
+                Messages.Count, correctMessages);
+        }
+
+        private (Dictionary<int, string> Rules, List<string> Messages) ParseInput(string filePath)
+        {
+            var input = File.ReadAllText(filePath);
+
+            var splitInput = input.Split("\r\n\r\n");
+
+            var rules = new Dictionary<int, string>();
+            var messages = splitInput[1].Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var line in splitInput[0].Split("\r\n"))
+            {
+                var (index, rule) = line.Extract<(int, string)>(@"(\d+): (.+)");
+
+                rules.Add(index, rule);
+            }
+
+            return (rules, messages.ToList());
+        }
+    }
+}
