640b472928af5c82e5736bae280ab408cb2a7fa2
Stefan van Oudenaarden <s.oudenaarden@gmail.com>
2021-01-07 19:09:30 +0100

    Added method to get all permutations of x items from a list.

--- Sharing is Caring/Extensions.cs
+++ Sharing is Caring/Extensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Advent
@@ -51,6 +52,18 @@ namespace Advent
             char[] array = input.ToCharArray();
             Array.Reverse(array);
             return new String(array);
+        }
+
+        public static IEnumerable<IEnumerable<T>> GetPermutations<T>(this IEnumerable<T> list, int length)
+        {
+            if (length == 1)
+            {
+                return list.Select(t => new T[] { t });
+            }
+
+            return GetPermutations(list, length - 1)
+                .SelectMany(t => list.Where(o => !t.Contains(o)),
+                    (t1, t2) => t1.Concat(new T[] { t2 }));
         }
     }
 }
