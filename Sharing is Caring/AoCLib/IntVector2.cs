3c88b2db81d67a341cf353341ed58393e35da69e
Stefan van Oudenaarden <s.oudenaarden@gmail.com>
2022-12-09 13:48:49 +0100

    AoC Library: Got frustrated at not having the kind of Vectory i like available. Made one.

---
+++ Sharing is Caring/AoCLib/IntVector2.cs
@@ -0,0 +1,122 @@
+﻿namespace Advent.AoCLib
+{
+    public class IntVector2
+    {
+        public int X;
+        public int Y;
+
+        public IntVector2()
+        {
+            X = 0;
+            Y = 0;
+        }
+
+        public IntVector2(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public IntVector2((int x, int y) input)
+        {
+            X = input.x;
+            Y = input.y;
+        }
+
+        public IntVector2(IntVector2 input)
+        {
+            X = input.X;
+            Y = input.Y;
+        }
+
+        public static IntVector2 operator -(IntVector2 a, IntVector2 b)
+            => new(a.X - b.X, a.Y - b.Y);
+        public static IntVector2 operator -(IntVector2 a, (int X, int Y) b)
+             => new(a.X - b.X, a.Y - b.Y);
+
+        public static IntVector2 operator -(IntVector2 a, int i)
+            => new(a.X - i, a.Y - i);
+
+        public static IntVector2 operator +(IntVector2 a, IntVector2 b)
+            => new(a.X + b.X, a.Y + b.Y);
+
+        public static IntVector2 operator +(IntVector2 a, (int X, int Y) b)
+            => new(a.X + b.X, a.Y + b.Y);
+
+        public static IntVector2 operator +(IntVector2 a, int i)
+            => new(a.X + i, a.Y + i);
+
+        public static IntVector2 operator *(IntVector2 a, IntVector2 b)
+            => new(a.X * b.X, a.Y * b.Y);
+
+        public static IntVector2 operator *(IntVector2 a, (int X, int Y) b)
+            => new(a.X * b.X, a.Y * b.Y);
+
+        public static IntVector2 operator *(IntVector2 a, int i)
+            => new(a.X * i, a.Y * i);
+
+        public static IntVector2 operator /(IntVector2 a, IntVector2 b)
+            => new(a.X / b.X, a.Y / b.Y);
+
+        public static IntVector2 operator /(IntVector2 a, (int X, int Y) b)
+            => new(a.X / b.X, a.Y / b.Y);
+
+        public static IntVector2 operator /(IntVector2 a, int i)
+            => new(a.X / i, a.Y / i);
+
+        public static bool operator ==(IntVector2 a, IntVector2 b)
+            => a.Equals(b);
+
+        public static bool operator ==(IntVector2 a, (int X, int Y) b)
+            => a.Equals(b);
+
+        public static bool operator !=(IntVector2 a, IntVector2 b)
+            => !a.Equals(b);
+
+        public static bool operator !=(IntVector2 a, (int X, int Y) b)
+            => !a.Equals(b);
+
+        public override string ToString()
+        {
+            return $"{X},{Y}";
+        }
+
+        public IntVector2 Copy => new(X, Y);
+                
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+
+            if (obj is null)
+            {
+                return false;
+            }
+
+            var vector = obj as IntVector2;
+            return X == vector.X && Y == vector.Y;
+        }
+
+        public bool Equals(IntVector2 vector)
+        {
+            return X == vector.X && Y == vector.Y;
+        }
+
+        public bool Equals((int X, int Y) tuple)
+        {
+            return X == tuple.X && Y == tuple.Y;
+        }
+
+        public bool Equals(int x, int y)
+        {
+            return X == x && Y == y;
+        }
+
+        public override int GetHashCode()
+        {
+            return X ^ Y; //Classic example used by the .net7 documentation for a two int hash
+        }
+    }
+}
