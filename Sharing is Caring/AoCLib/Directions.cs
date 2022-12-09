228062ad064870924b3daefb702cae9a5802cbf0
Stefan van Oudenaarden <s.oudenaarden@gmail.com>
2022-12-09 13:48:16 +0100

    AoC Library: These should probably start living outside of grid.

---
+++ Sharing is Caring/AoCLib/Directions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Advent.AoCLib
+{
+    public static class Directions
+    {
+        public static readonly (int x, int y) Right = (0, 1);
+        public static readonly (int x, int y) Left = (0, -1);
+        public static readonly (int x, int y) Down = (1, 0);
+        public static readonly (int x, int y) DownRight = (1, 1);
+        public static readonly (int x, int y) DownLeft = (1, -1);
+        public static readonly (int x, int y) Up = (-1, 0);
+        public static readonly (int x, int y) UpRight = (-1, 1);
+        public static readonly (int x, int y) UpLeft = (-1, -1);
+    }
+}
